b0089e6c1e80af6799fbbc31ae4c1a1862e4d595
Roman Inflianskas <infroma@gmail.com>
2018-09-29 12:09:01 +0300

    Set variables globaly (see https://github.com/oh-my-fish/oh-my-fish#setting-variables-in-initfish)

--- conf.d/pyenv.fish
+++ conf.d/pyenv.fish
@@ -7,13 +7,13 @@ set -l pyenv_root ""
 
 if test -z "$PYENV_ROOT"
     set pyenv_root ~/.pyenv
-    set -x PYENV_ROOT "$pyenv_root"
+    set -xg PYENV_ROOT "$pyenv_root"
 else
     set pyenv_root "$PYENV_ROOT"
 end
 
 if status --is-login
-    set -x PATH "$pyenv_root/shims" $PATH
-    set -x PYENV_SHELL fish
+    set -xg PATH "$pyenv_root/shims" $PATH
+    set -xg PYENV_SHELL fish
 end
 command mkdir -p "$pyenv_root/"{shims,versions}
